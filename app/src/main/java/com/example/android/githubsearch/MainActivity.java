11a33a37a27c81abaaa22f7e6b8ae2491a670c69
Carson P <PembleC@oregonstate.edu>
2020-01-31 11:24:16 -0800

    Finishing API calls with Async Tasks

--- app/src/main/java/com/example/android/githubsearch/MainActivity.java
+++ app/src/main/java/com/example/android/githubsearch/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -12,7 +13,9 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import com.example.android.githubsearch.utils.GitHubUtils;
+import com.example.android.githubsearch.utils.NetworkUtils;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -56,12 +59,52 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String searchQuery = mSearchBoxET.getText().toString();
                 if (!TextUtils.isEmpty(searchQuery)) {
-                    mGitHubSearchAdapter.updateSearchResults(new ArrayList<String>(Arrays.asList(dummySearchResults)));
-                    String url = GitHubUtils.buildGitHubSearchURL(searchQuery);
-                    Log.d(TAG, "querying url: " + url);
-                    mSearchBoxET.setText("");
+                    doGitHubSearch(searchQuery);
+                    //mGitHubSearchAdapter.updateSearchResults(new ArrayList<String>(Arrays.asList(dummySearchResults)));
                 }
             }
         });
     }
+
+    private void doGitHubSearch (String searchQueries){
+        String url = GitHubUtils.buildGitHubSearchURL(searchQueries);       //Builds the URL in Github Utils
+        Log.d(TAG, "querying url: " + url);
+        new GitHubSearchTask().execute(url);
+
+
+    }
+
+
+    // Pass in a String, Progress Bar=void, Returns JSON String
+    public class GitHubSearchTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {    // Variable number of arguments;
+            String url = strings[0];                            // Arguments are stored as array
+            String searchResults = null;
+            try {
+                searchResults = NetworkUtils.doHttpGet(url);    // Do HTTP Call
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return searchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if (s != null){
+                ArrayList<String> searchResultsList = new ArrayList<>();        // Create a new Array
+                searchResultsList.add(s);                                       // Add output to the array
+                mGitHubSearchAdapter.updateSearchResults(searchResultsList);    // Update the results
+                mSearchBoxET.setText("");                                       // Clear text box
+
+
+            }
+
+        }
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/android/githubsearch/utils/NetworkUtils.java
+++ app/src/main/java/com/example/android/githubsearch/utils/NetworkUtils.java
@@ -1,5 +1,7 @@
 package com.example.android.githubsearch.utils;
 
+import java.io.IOException;
+
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -7,7 +9,7 @@ import okhttp3.Response;
 public class NetworkUtils {
     private static final OkHttpClient mHTTPClient = new OkHttpClient();
 
-    public static String doHttpGet(String url) {
+    public static String doHttpGet(String url) throws IOException {
         Request request = new Request.Builder().url(url).build();
         Response response = mHTTPClient.newCall(request).execute();
         try {
